3984103c3643dfd9d771e3519f34e8f6db4f3f7e
mataguirre <mataguirre.dev@gmail.com>
2023-01-25 18:10:38 -0300

    react-proyect

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+//importo react para trabajar en navegadores
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello world</h1>);
